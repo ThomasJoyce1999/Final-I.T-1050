449e290e89bc98267f01aa6eab921692153ef2a8
ThomasJoyce1999 <35536331+ThomasJoyce1999@users.noreply.github.com>
2018-05-08 13:50:12 -0400

    Final
    Final

---
+++ Final/ConsoleApplication2/Elevator.cs
@@ -0,0 +1,35 @@
+﻿namespace Final
+{
+    class Elevator
+    {
+        double MaxWeight;
+        Passenger[] Occupants;
+
+        public Elevator(int maxOccupants, double maxWeight)
+        {
+            this.Occupants = new Passenger[maxOccupants];
+            this.MaxWeight = maxWeight;
+        }
+
+        public void AddOccupant(Passenger passenger, int index)
+        {
+            this.Occupants[index] = passenger;
+        }
+
+        public double GetCurrentWeight()
+        {
+            double CurrentWeight = 0;
+            foreach (Passenger passenger in Occupants)
+            {
+                CurrentWeight += passenger.GetWeight();
+            }
+            return CurrentWeight;
+        }
+
+        public bool IsOverMaxCapacity()
+        {
+            return GetCurrentWeight() > MaxWeight;
+        }
+    }
+
+}
